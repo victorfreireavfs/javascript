5a4a6fe90f335adc17c2c36f019f73bf51dfce89
victorfreireavfs <victoravfs@gmail.com>
2023-11-07 22:35:40 -0300

    condições em JS

---
+++ exercícios/aula08/ex007.js
@@ -0,0 +1,7 @@
+
+var vel =78
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+if(vel > 60){ //Condição simples!
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`Diriga sempre usando cinto de segurança!`)
