1a0350922bff5c36da7becda532fd38dec8d4e4c
victorfreireavfs <victoravfs@gmail.com>
2023-12-14 22:18:01 -0300

    exercicio

--- exercícios/aula11ex/ex15/script.js
+++ exercícios/aula11ex/ex15/script.js
@@ -1,21 +1,20 @@
-function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-  
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12){
-        //BOM DIA!
-        document.body.style.background = '#e2cd9f'
-        img.src = 'imagens/manhã.png'
-    } else if (hora >= 12 && hora< 18){
-        //BOA TARDE!
-        document.body.style.background = '#b9846f'
-        img.src = 'imagens/tarde.png'
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
     } else{
-        //BOA NOITE!
-        document.body.style.background = '#515154'
-        img.src = 'imagens/noite.png'
+        res.innerHTML = 'Contando:'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c = i; c <= f; c +=p ){
+            res.innerHTML += `${c}`
+        }
     }
+
 }
