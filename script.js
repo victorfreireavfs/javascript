05f1cbad272c9fa607a4b1624090c0e5bdb6eaf0
victorfreireavfs <victoravfs@gmail.com>
2023-12-20 21:34:32 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+/*var result = 1
+var conter = 0
+while(conter <10){
+    result = result * 2
+    conter = conter + 1
+}
+
+console.log(result)*/
+
+/*for (var number = 0; number <= 12; number = number + 2)
+console.log(number)*/
+
+
+//LOOP
+/*var result = 1
+for (var counter = 0; counter < 10; counter = counter + 1)
+result = result * 2
+console.log(result)*/
+
+// ATUALIZANDO VARIÁVEIS
+
+//for(let line = '#'; line.length < 10; line += '#') console.log(line)
+
+// comprimento da string = length
